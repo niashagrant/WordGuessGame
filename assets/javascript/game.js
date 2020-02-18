3fb1203f0528cb758bec0c3460fda124a9bedf6c
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-02-18 03:13:18 -0700

    made the rough page with bootstrap, started the the js page with words that I want to use for the hangman.

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+// alert("hello all");
+
+// $("button").click(function() {
+//   if (this.id == "showGamePage") {
+//     $("#instructions").show();
+//     $("#playGame").hide();
+//   } else {
+//     $("#instructions").hide();
+//     $("#playGame").show();
+//   }
+// });
+
+// $("#showGamePage").click(function() {
+//   $("#instructions".css("display") == "none");
+// });
+
+var desserts = [
+  [
+    "cake",
+    "cinnamon roll",
+    "cookie",
+    "brownie",
+    "muffin",
+    "cupcake",
+    "donut",
+    "doughnut",
+    "flan",
+    "fudge",
+    "frozen yogurt",
+    "gingerbread",
+    "macaron",
+    "brittle",
+    "popsicle",
+    "pie",
+    "pudding",
+    "sundae",
+    "tart",
+    "souffle",
+    "meringue"
+  ]
+];
+// var randomDessertArray = dessert[Math.floor(Math.random() * desserts.length)];
+// var randomDessert = randomDessertArray[
+//   Math.floor(Math.random() * randomDessertArray.length)
+// ].toUpperCase();

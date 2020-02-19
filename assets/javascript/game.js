b4a991d1db2fbd759649e98c6203ed6c178f2084
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-02-19 01:23:41 -0700

    was able to get the random word to show up onto the html page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,33 +1,92 @@
+var dessertWordBank = [
+  "cake",
+  "cinnamon roll",
+  "cookie",
+  "brownie",
+  "muffin",
+  "cupcake",
+  "donut",
+  "doughnut",
+  "flan",
+  "fudge",
+  "frozen yogurt",
+  "gingerbread",
+  "macaron",
+  "brittle",
+  "popsicle",
+  "pie",
+  "pudding",
+  "sundae",
+  "tart",
+  "souffle",
+  "meringue"
+];
+
+var word = [];
+var wrongGuess = [];
+var rightGuess = [];
+// var i = 1;
+// var y = 0;
+
+var letters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "_"
+];
+
 $(document).ready(function() {
-  var letters = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-    "_"
-  ];
+  $("#showGamePage").click(function() {
+    $("#instructions").hide();
+    $("#playGame").show();
+  });
+
+  function randomWord() {
+    var random = Math.floor(Math.random() * dessertWordBank.length);
+    var toString = dessertWordBank[random];
+    console.log(toString);
+    word = toString.split("");
+    console.log(word);
+  }
+  randomWord();
+
+  function wordSpaces() {
+    for (var i = 0; i < word.length; i++) {
+      $("#displayLetters").append("<li>" + word[i] + "</li>");
+    }
+  }
+  wordSpaces();
+
+  function play(space) {
+    var index = jQuery.inArray(space, word);
+    if (index === -1) {
+      wrongGuess.push(space);
+      var wrong = wrongGuess.length;
+    }
+  }
 
   for (i = 0; i < letters.length; i++) {
     var letterBtn = $("<button>");
@@ -42,43 +101,5 @@ $(document).ready(function() {
     fridgeMagnet.addClass("letter fridge-color");
     fridgeMagnet.text($(this).attr("data-letter"));
     $("#display").append(fridgeMagnet);
-  });
-});
-
-// var desserts = [
-//   [
-//     "cake",
-//     "cinnamon roll",
-//     "cookie",
-//     "brownie",
-//     "muffin",
-//     "cupcake",
-//     "donut",
-//     "doughnut",
-//     "flan",
-//     "fudge",
-//     "frozen yogurt",
-//     "gingerbread",
-//     "macaron",
-//     "brittle",
-//     "popsicle",
-//     "pie",
-//     "pudding",
-//     "sundae",
-//     "tart",
-//     "souffle",
-//     "meringue"
-//   ]
-// ];
-
-// var randomDessertArray = dessert[Math.floor(Math.random() * desserts.length)];
-// var randomDessert = randomDessertArray[
-//   Math.floor(Math.random() * randomDessertArray.length)
-// ].toUpperCase();
-
-$(document).ready(function() {
-  $("#showGamePage").click(function() {
-    $("#instructions").hide();
-    $("#playGame").show();
   });
 });
